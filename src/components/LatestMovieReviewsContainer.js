1a7ff4a2c09d39fafa42707910564c14bed39644
Eric <eric.m.lombardo@gmail.com>
2021-08-07 14:05:46 -0400

    Build container and presentation components

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,35 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = process.env.REACT_APP_NYT_KEY;
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {
+  
+  state = {
+    reviews: []
+  }
+
+  componentDidMount() {
+    fetch(`${URL}`)
+    .then(resp => resp.json())
+    .then(reviews => this.assignReviews(reviews))
+  }
+
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+        <h1>Latest Movies Start Here</h1>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+
+  assignReviews = (reviews) => {
+    this.setState({
+      reviews: reviews.results
+    })
+  }
+}
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,47 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = process.env.REACT_APP_NYT_KEY;
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+  
+  state = {
+    reviews: [],
+    searchTerm: ''
+  }
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <form id="search-form" onSubmit={this.handleSearch}>
+          <label htmlFor="search">Search: </label> 
+          <input id="search" type="text" onChange={this.handleChange}/>
+          <button type="submit">Search</button>
+        </form>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    )
+  }
+
+  assignReviews = (reviews) => {
+    this.setState({
+      reviews: reviews.results
+    })
+  }
+
+  handleSearch = (e) => {
+    e.preventDefault()
+    fetch(URL + `&query=${this.state.searchTerm}`)
+    .then(resp => resp.json())
+    .then(reviews => this.assignReviews(reviews))
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      searchTerm: e.target.value
+    })
+  }
+}
