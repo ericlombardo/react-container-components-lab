affc7604cf35ebeb9d0667251b0bf06da946adc4
Eric <eric.m.lombardo@gmail.com>
2021-08-07 14:04:07 -0400

    commit tree dee5d3efb9d5622872b6f0fbda50b3579aa51208

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,30 @@
-// Code MovieReviews Here
+// stateless function
+import React from 'react'
+
+const MovieReviews = (props) => {
+  return (
+    <div className="review-list">
+      {props.reviews.map(review => CreateReview(review))}
+    </div>
+  )
+}
+
+const CreateReview = ({ // checked solution to see this seperation of concerns from MovieReviews Component
+  display_title,
+  mpaa_rating,
+  summary_short,
+  multimedia
+}) => {
+  return (
+    <div key={display_title} className="review">
+      <img src={multimedia.src} alt=""></img>
+      <h1>{display_title}</h1>
+      <p>{mpaa_rating}</p>
+      <p>{summary_short}</p>
+    </div>
+  )
+}
+
+MovieReviews.defaultProps = []
+
+export default MovieReviews
